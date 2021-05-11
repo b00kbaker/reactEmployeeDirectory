ce1b33546daae007fbe268a0153387835796e0b9
b00kbaker <caimmurphy@gmail.com>
2021-05-11 12:36:01 -0600

    API pseudo and started index.js

---
+++ utils/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default{
+ getRandomPeople: function(){
+    // use randomAPI to get (x) amount of results to populate page
+ },
+ getPeopleNames: function(names){
+ //    as the user types filter through the names of the employees for matches
+ },
+ getPhoneNumber: function(phone){
+ // as the user types numbers filter for matching phone digits
+ }
+
+};
+
+// api call results.name
+//   results.email
+//   results.phone
+//   results.picture.thumbnail
+//   results.info.seed?
+
+
+// API key: HG6F-T9JG-NAFQ-KJKA
